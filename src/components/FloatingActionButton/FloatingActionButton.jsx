6d32868b759bdb627eef523b534e902b26be5d2e
Vang Vang <vang2code@gmail.com>
2023-06-09 14:35:11 -0500

    applied dialog box with text field of select and text box

--- src/components/FloatingActionButton/FloatingActionButton.jsx
+++ src/components/FloatingActionButton/FloatingActionButton.jsx
@@ -4,13 +4,33 @@ import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 //zoom import
 import Zoom from '@mui/material/Zoom';
-
+//dialog imports
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+//text field import
+import MenuItem from '@mui/material/MenuItem';
 
 export default function FloatingActionButton() {
 
+    //handle opening of dialog box
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
   return (
     <div>
-      <Box sx={{ '& > :not(style)': { m: 1 } }}>
+      <Box sx={{ '& > :not(style)': { m: 1 } }} onClick={handleClickOpen}>
         <Zoom
         in = {true}
         timeout = {{enter: 500, exit: 500}}
@@ -33,6 +53,56 @@ export default function FloatingActionButton() {
           </Fab>
         </Zoom>
       </Box>
+      {/* dialog box */}
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Add New Log</DialogTitle>
+        <DialogContent>
+          
+          {/* Date */}
+
+          {/* Activities Select Field */}
+          <TextField
+          select
+          label="Activities"
+          helperText="Please select your activities"
+        ></TextField>
+        {/* Skills Select Field */}
+        <TextField
+          select
+          label="Skills"
+          helperText="Please select your skills"
+        ></TextField>
+        {/* text fields that auto field with the XP they get */}
+
+        {/*  Source text field */}
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Source"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+          {/* Takeaways text field */}
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Takeaways"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Submit</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
+
+
+
