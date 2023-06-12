0249f7f9e0b0b8fe0578705698f02f03416f2412
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-06-12 13:54:53 -0500

    Built user-activites reducer and saga files.

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -4,6 +4,7 @@ import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
 import activitySaga from './activities.saga';
 import skillsSaga from './skills.saga'
+import userActivitiesSaga from './user-activities.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -19,5 +20,6 @@ export default function* rootSaga() {
     userSaga(),
     activitySaga(),
     skillsSaga(),
+    userActivitiesSaga(),
   ]);
 }
