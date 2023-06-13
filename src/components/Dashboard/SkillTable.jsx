cc6e6bc338a8c41950999c0333976daed9b85b80
Anders Boyum <Acboyum@gmail.com>
2023-06-13 16:18:11 -0500

    created calculateLevel function, successfully filtered the user's activities by skill name

--- src/components/Dashboard/SkillTable.jsx
+++ src/components/Dashboard/SkillTable.jsx
@@ -1,4 +1,5 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 // MUI Components
 import Table from '@mui/material/Table';
@@ -10,21 +11,29 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 
- // * What does this component need to do?
-    // * 1. It needs a "Skills" Header
-    // * 2. It needs to map through and list each user skill
-        // * a. Each skill needs it's current level, current XP, XP to next Level, and Badge
-
         // How do we calculate the level, current XP / XP to next level, etc. 
         // How do we do badges?
 function SkillTable() {
     // things needed from the store to calculate the user's skill levels
     const skills = useSelector(store => store.skills);
-    const activities = useSelector(store => store.activites);
+    const activities = useSelector(store => store.activities);
     const userActivities = useSelector(store => store.userActivities);
+    console.log("Activities, userActivities", activities, userActivities);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch({type: 'FETCH_USER_ACTIVITIES'})
+    }, [])
+
+    // need to calculate the user's skill levels 
+    const calculateLevel = (skill) => {
+        const actInstances = userActivities.filter(item => skill.skill_name === item.skills_enterprise_name || skill.skill_name === item.skills_user_name)
+        // console.log("Every instance of this thing", actInstances)
+        return actInstances.length;
+    }
 
 
-    console.log("Skills", skills);
     return(
         <div id="dboard-skill-table">
             <h2>Skills</h2>
@@ -48,7 +57,7 @@ function SkillTable() {
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                     >
                                         <TableCell>{skill.skill_name}</TableCell>
-                                        <TableCell></TableCell>
+                                        <TableCell>{calculateLevel(skill)}</TableCell>
                                         <TableCell></TableCell>
                                         <TableCell></TableCell>
                                         <TableCell></TableCell>
@@ -63,20 +72,3 @@ function SkillTable() {
     )
 }
 export default SkillTable;
-
-{/* <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Skill</TableCell>
-            <TableCell align="right">Level</TableCell>
-            <TableCell align="right">Total XP</TableCell>
-            <TableCell align="right">XP Until Next Level</TableCell>
-            <TableCell align="right">Badge</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        //   Here goes the skills
-        </TableBody>
-      </Table>
-    </TableContainer> */}
