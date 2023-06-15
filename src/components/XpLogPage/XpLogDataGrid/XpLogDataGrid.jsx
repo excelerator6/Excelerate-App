50cfc319646e740401fdfc1f029df0c4fb2fb5bb
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-06-15 11:05:02 -0500

    XpLogDataGrid updates
    - Moved the wrapping function to encompase the columns
    - Added a "Delete All" button
    	- not yet hooked up to actually delete anything
    	- captures the ids of the rows that have been selected.

--- src/components/XpLogPage/XpLogDataGrid/XpLogDataGrid.jsx
+++ src/components/XpLogPage/XpLogDataGrid/XpLogDataGrid.jsx
@@ -1,87 +1,97 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
 import { useDispatch, useSelector } from "react-redux";
 
-const columns = [
-  // { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'date',
-    renderHeader: () => (
-      <strong>{'Date'}</strong>
-    ),
-    minWidth: 100,
-    flex: .4,
-    editable: true,
-  },
-  {
-    field: 'skill',
-    renderHeader: () => (
-      <strong>{'Skill'}</strong>
-    ),
-    minWidth: 120,
-    flex: .8,
-    editable: false,
-  },
-  {
-    field: 'activity',
-    renderHeader: () => (
-      <strong>{'Activity'}</strong>
-    ),
-    minWidth: 130,
-    flex: 1,
-    editable: false,
-  },
-  {
-    field: 'xp',
-    renderHeader: () => (
-      <strong>{'XP'}</strong>
-    ),
-    numer: true,
-    minWidth: 30,
-    flex: .2,
-    editable: false,
-  },
-  {
-    field: 'source',
-    renderHeader: () => (
-      <strong>{'Source'}</strong>
-    ),
-    minWidth: 130,
-    flex: 1,
-    editable: false,
-  },
-  {
-    field: 'takeaways',
-    renderHeader: () => (
-      <strong>{'Key Takeaways'}</strong>
-    ),
-    minWidth: 130,
-    flex: 1,
-    editable: false,
-  },
-];
-
 export default function XpLogDataGrid() {
   const userActivities = useSelector(store => store.userActivities);
-  console.log(userActivities);
+  const columns = [
+    // { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'date',
+      renderHeader: () => (
+        <strong>{'Date'}</strong>
+      ),
+      minWidth: 100,
+      flex: .4,
+      editable: true,
+    },
+    {
+      field: 'skill',
+      renderHeader: () => (
+        <strong>{'Skill'}</strong>
+      ),
+      minWidth: 120,
+      flex: .8,
+      editable: false,
+    },
+    {
+      field: 'activity',
+      renderHeader: () => (
+        <strong>{'Activity'}</strong>
+      ),
+      minWidth: 130,
+      flex: 1,
+      editable: false,
+    },
+    {
+      field: 'xp',
+      renderHeader: () => (
+        <strong>{'XP'}</strong>
+      ),
+      numer: true,
+      minWidth: 50,
+      flex: .2,
+      editable: false,
+    },
+    {
+      field: 'source',
+      renderHeader: () => (
+        <strong>{'Source'}</strong>
+      ),
+      minWidth: 130,
+      flex: 1,
+      editable: false,
+    },
+    {
+      field: 'takeaways',
+      renderHeader: () => (
+        <strong>{'Key Takeaways'}</strong>
+      ),
+      minWidth: 130,
+      flex: 1,
+      editable: false,
+    },
+  ];
+  const [selectedIds, setSelectedIds] = useState([]);
+
+  const handleDeleteSelected = () => {
+    console.log('ids to delete', selectedIds);
+  }
+
+  // console.log(userActivities);
   return (
     <Box sx={{ height: 540, width: '100%' }}>
       <DataGrid
         rows={userActivities}
         columns={columns}
-        getRowHeight={() => 'auto'}
+        // rowHeight={50}
+        // getRowHeight={() => 'auto'}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: 5,
+              pageSize: 10,
             },
           },
         }}
         pageSizeOptions={[5, 10, 25, 50]}
         checkboxSelection
         disableRowSelectionOnClick
+        onRowSelectionModelChange={(ids) => {
+          setSelectedIds(ids)
+        }}
       />
+      <button onClick={handleDeleteSelected}>Delete Selected</button>
     </Box>
   );
 }
