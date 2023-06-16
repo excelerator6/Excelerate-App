40facb09f00d376a8d4b78bd1b147cae79a482fa
Anders Boyum <Acboyum@gmail.com>
2023-06-16 15:59:03 -0500

    got the calendar to correctly display activities based on activity log date

--- src/components/StatsPage/Calendar.jsx
+++ src/components/StatsPage/Calendar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 // calendar plugins
 import FullCalendar from '@fullcalendar/react';
@@ -6,9 +7,27 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import { Paper } from '@mui/material';
 
 function Calendar(){
-    const userActivites = useSelector(store => store.userActivites);
+    const [activitiesReceived, setActivitiesReceived] = useState(false);
 
+    const userActivities = useSelector(store => store.userActivities);
 
+    const formatDate = (date) => {
+        const year = date.slice(-4);
+        const month = date.slice(0, 2);
+        const day = date.slice(3, 5);
+
+        return `${year}-${month}-${day}`;
+    } 
+
+    const formattedActivities = userActivities.map(item => {
+        return{
+            // id: `${item.id}`,
+            date: `${formatDate(item.date)}`,
+            title:`${item.activity} / ${item.source}`
+        }
+    }) 
+
+    console.log(userActivities)
     return(
         <div id='fullCalendar'>
             <Paper>
@@ -16,10 +35,12 @@ function Calendar(){
                     plugins={[ dayGridPlugin ]}
                     initialView="dayGridMonth"
                     aspectRatio={2.4}
+                    events={formattedActivities}
                 />
             </Paper>
         </div>
     )
+    
 }
 
 export default Calendar;
