f49b9183f333f634afa1e3a7dfa951f035de7689
Vang Vang <vang2code@gmail.com>
2023-06-19 10:47:28 -0500

    added avatars to the app.jsx file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,46 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import {
   HashRouter as Router,
   Redirect,
   Route,
   Switch,
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
-import Nav from '../Nav/Nav';
-import Footer from '../Footer/Footer';
+import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
 
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 
-import AboutPage from '../AboutPage/AboutPage';
-import Dashboard from '../Dashboard/Dashboard';
-import InfoPage from '../InfoPage/InfoPage';
-import LandingPage from '../LandingPage/LandingPage';
-import LoginPage from '../LoginPage/LoginPage';
-import RegisterPage from '../RegisterPage/RegisterPage';
+import AboutPage from "../AboutPage/AboutPage";
+import Dashboard from "../Dashboard/Dashboard";
+import InfoPage from "../InfoPage/InfoPage";
+import LandingPage from "../LandingPage/LandingPage";
+import LoginPage from "../LoginPage/LoginPage";
+import RegisterPage from "../RegisterPage/RegisterPage";
 
 //importing floatingActionButton
-import FloatingActionButton from '../FloatingActionButton/FloatingActionButton';
+import FloatingActionButton from "../FloatingActionButton/FloatingActionButton";
 //importing to wrap app for the date picker
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
 //import app css file
-import './App.css';
-//import avatars 
-import Avatars from '../Avatars/Avatars';
+import "./App.css";
+//import avatars
+import Avatars from "../Avatars/Avatars";
 
-import XpLogPage from '../XpLogPage/XpLogPage';
-import StatsPage from '../StatsPage/StatsPage';
+import XpLogPage from "../XpLogPage/XpLogPage";
+import StatsPage from "../StatsPage/StatsPage";
 
 function App() {
   const dispatch = useDispatch();
 
-  const user = useSelector(store => store.user);
+  const user = useSelector((store) => store.user);
 
   useEffect(() => {
-    dispatch({ type: 'FETCH_USER' });
+    dispatch({ type: "FETCH_USER" });
   }, [dispatch]);
 
   return (
@@ -60,7 +60,14 @@ function App() {
             >
               <AboutPage />
             </Route>
-
+            {/* avatars page */}
+            <ProtectedRoute
+              // logged in shows UserPage else shows LoginPage
+              exact
+              path="/avatars"
+            >
+              <Avatars />
+            </ProtectedRoute>
             {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
@@ -81,66 +88,48 @@ function App() {
               <InfoPage />
             </ProtectedRoute>
 
-            <Route
-              exact
-              path="/login"
-            >
-              {user.id ?
-                // If the user is already logged in, 
+            <Route exact path="/login">
+              {user.id ? (
+                // If the user is already logged in,
                 // redirect to the /user page
                 <Redirect to="/user" />
-                :
+              ) : (
                 // Otherwise, show the login page
                 <LoginPage />
-              }
+              )}
             </Route>
 
-            <Route
-              exact
-              path="/registration"
-            >
-              {user.id ?
-                // If the user is already logged in, 
+            <Route exact path="/registration">
+              {user.id ? (
+                // If the user is already logged in,
                 // redirect them to the /user page
                 <Redirect to="/user" />
-                :
+              ) : (
                 // Otherwise, show the registration page
                 <RegisterPage />
-              }
+              )}
             </Route>
 
-            <Route
-              exact
-              path="/home"
-            >
-              {user.id ?
-                // If the user is already logged in, 
+            <Route exact path="/home">
+              {user.id ? (
+                // If the user is already logged in,
                 // redirect them to the /user page
                 <Redirect to="/user" />
-                :
+              ) : (
                 // Otherwise, show the Landing page
                 <LandingPage />
-              }
+              )}
             </Route>
 
-            <ProtectedRoute
-              exact
-              path='/xp-log'
-            >
+            <ProtectedRoute exact path="/xp-log">
               <XpLogPage />
             </ProtectedRoute>
 
-            <ProtectedRoute
-              exact
-              path='/StatsPage'
-            >
+            <ProtectedRoute exact path="/StatsPage">
               <StatsPage />
             </ProtectedRoute>
 
-            <ProtectedRoute
-              exact
-              path='/Dashboard'
-            >
+            <ProtectedRoute exact path="/Dashboard">
               <Dashboard />
             </ProtectedRoute>
 
