e8e8146efd549f8d2c215f1dd232046a901935d3
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-06-21 10:08:10 -0500

    AchievementsPage updates
    - XpEarnedTab.jsx changes
            - Now maps through the achievements related to XpEarned
            - Displays those achievements in a grid
                    - This grid still needs to be built out with cards for each achievement
                      and either a grey star or a gold star depending on if it has been completed or not.
    
    - AchievementsTabs changes
            - Removed import of Typography as no longer utilizing within this page
    
    - achievements.reducer.js
            - Renamed user-achievements.reducer to achievements.reducer.js and now have it split between
              allAchievementsReducer and userAchievementsReducer functions inside, and then exported together
              with combineReducers

--- src/components/AchievementsPage/AchievementsTabs/TabViews/XpEarnedTab.jsx
+++ src/components/AchievementsPage/AchievementsTabs/TabViews/XpEarnedTab.jsx
@@ -1,10 +1,25 @@
-import { useSelector } from "react-redux"
-import StarIcon from '@mui/icons-material/Star';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { Card, CardContent, Grid, Typography } from "@mui/material";
 
 export default function XpEarnedTab() {
+  const xpEarned = useSelector(store => store.achievements.allAchievementsReducer.xpEarned)
+  console.log(xpEarned);
+
   return (
     <>
-      <h3>Inside Xp Earned Tab <StarIcon /></h3>
+      <h3>Inside Xp Earned Tab</h3>
+      <h4>Achievements:</h4>
+      <Grid container spacing={2}>
+        {xpEarned && xpEarned.length > 0 ?
+          xpEarned.map((achievement, index) => (
+            <Grid item xs={3} key={index}>
+              {achievement}
+            </Grid>
+          ))
+          : ''
+        }
+      </Grid>
     </>
   )
 }
