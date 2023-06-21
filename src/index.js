53ef731095a481d5a781905d80ebd0b792c90c11
Anders Boyum <Acboyum@gmail.com>
2023-06-21 16:00:08 -0500

    got an initial theme provided to the app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { createTheme, ThemeProvider } from '@mui/material'; 
 
 import store from './redux/store';
 
 import App from './components/App/App';
+
+const exceleratorTheme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#303841',
+    },
+    secondary: {
+      main: '#90ee90',
+    },
+    success: {
+      main: '#ffe801',
+    },
+    typography: {
+      fontFamily: []
+    },
+    // divider: 'rgba(255,255,255,0.12)',
+  }
+})
 
 
 const root = ReactDOM.createRoot(document.getElementById('react-root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={exceleratorTheme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
