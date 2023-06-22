757014b53483490c6067e3b12f9e68dc5e4e6cb8
Vang Vang <vang2code@gmail.com>
2023-06-22 13:05:31 -0500

    added padding on top from avatar.css and centered avatar and button to the card, added highlight cards when hover and change button color when hover

--- src/components/Avatars/AvatarsItem.jsx
+++ src/components/Avatars/AvatarsItem.jsx
@@ -4,12 +4,11 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 //import for material ui button
 import Button from "@mui/material/Button";
-import { Box, Card, Grid } from "@mui/material";
+import { Box, Card, Grid, CardActionArea } from "@mui/material";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 
 function AvatarsItem() {
-
   const dispatch = useDispatch();
   //handleUpdate takes in 2 parameters, the new avatarOptions and dispatch called when clicked on
   const handleUpdate = (event, avatarNewName, message) => {
@@ -44,53 +43,53 @@ function AvatarsItem() {
   ];
 
   return (
-    <Box 
-    sx={{ flexGrow: 1 }}
-    >
+    <Box sx={{ flexGrow: 1 }} style={{ padding: 10 }}>
       <Grid
         container
-        spacing={3}
-        // spacing={{ xs: 2, md: 3 }}
-        // columns={{ xs: 4, sm: 8, md: 12 }}
-
-        // direction="row"
+        direction="row"
+        spacing={{ xs: 3, md: 4 }}
+        columns={{ xs: 3, sm: 8, md: 12 }}
         justifyContent="center"
         alignItems="center"
+        textAlign="center"
       >
         {avatarOptions.map((option, index) => {
           return (
-            <Grid
-            item
-            //   columnSpacing={{ xs: 1, sm: 2, md: 1 }}
-            //   xs={2}
-            //   sm={4}
-            //   md={4}
-            //   key={index}
-            >
-              <Card 
-            //   sx={{ }} 
-            //   variant="outlined"
-              >
-                <CardContent>
-                  <Avatar
-                    size={100}
-                    variant="beam"
-                    colors={colors}
-                    name={option}
-                  />
-                </CardContent>
-                <CardActions>
-                  <Button
-                    size="lg"
-                    variant="outlined"
-                    sx={{ borderline: "" }}
-                    onClick={(event) => {
-                      handleUpdate(event, option, "UPDATE_AVATAR_NAME");
-                    }}
-                  >
-                    Click Here!
-                  </Button>
-                </CardActions>
+            <Grid item xs={3} key={index}>
+              <Card sx={{ maxWidth: 1, minWidth: 1  }} variant="outlined">
+                <CardActionArea>
+                  <CardContent>
+                    <Avatar
+                      size={100}
+                      variant="beam"
+                      colors={colors}
+                      name={option}
+                    />
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      style={{ margin: "0 auto", display: "flex" }}
+                      justifyContent="center"
+                      alignItems="center"
+                      textAlign="center"
+                      size="lg"
+                      variant="outlined"
+                      sx={[
+                        {
+                          "&:hover": {
+                            color: "#36454f",
+                            backgroundColor: "#90ee90",
+                          },
+                        },
+                      ]}
+                      onClick={(event) => {
+                        handleUpdate(event, option, "UPDATE_AVATAR_NAME");
+                      }}
+                    >
+                      Click Here!
+                    </Button>
+                  </CardActions>
+                </CardActionArea>
               </Card>
             </Grid>
           );
