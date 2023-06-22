eac83849e7477a9f82c79d9c90f7cbf393e27169
Vang Vang <vang2code@gmail.com>
2023-06-22 10:55:50 -0500

    able to get the saga, reducer, router working with database, avatar will changed to new avatar picture when clicked in the dom

--- src/redux/sagas/avatars.saga.js
+++ src/redux/sagas/avatars.saga.js
@@ -1,28 +1,24 @@
 import axios from "axios";
 import { put, takeLatest } from "redux-saga/effects";
 
-
-
 function* putAvatarName(action) {
   try {
-    const updateAvatarName = action.payload;
-    console.log("updateAvatarName******", updateAvatarName);
+    const avatarNewName = action.payload;
 
     const response = yield axios({
       method: "PUT",
-      url: `/api/avatars/${updateAvatarName.id}`,
-      data: updateAvatarName,
+      url: `/api/avatars/`,
+      data: { avatarNewName },
     });
-    // yield put({
-    //   type: "FETCH_AVATAR",
-    // });
+    yield put({
+      type: "FETCH_USER",
+    });
   } catch (error) {
     console.log("putAvatarName axios PUT failed!!", error);
   }
 }
 
 function* avatars() {
-//   yield takeLatest("FETCH_AVATAR", getAvatar);
   yield takeLatest("UPDATE_AVATAR_NAME", putAvatarName);
 }
 
