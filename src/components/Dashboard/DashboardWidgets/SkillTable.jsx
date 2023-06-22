a2e099f47e7bbfde5336dc8b2fcbf95a7d05f5f3
Anders Boyum <Acboyum@gmail.com>
2023-06-22 16:24:12 -0500

    changed the logo color and reverted nav and app bars back to the original gray:

--- src/components/Dashboard/DashboardWidgets/SkillTable.jsx
+++ src/components/Dashboard/DashboardWidgets/SkillTable.jsx
@@ -63,7 +63,7 @@ function SkillTable() {
         },
         [`& .${linearProgressClasses.bar}`]: {
           borderRadius: 10,
-          backgroundColor: theme.palette.mode === 'light' ? '#90ee90' : '#308fe8',
+          backgroundColor: theme.palette.mode === 'light' ? '#5982c4' : '#308fe8',
         },
       }));
 
