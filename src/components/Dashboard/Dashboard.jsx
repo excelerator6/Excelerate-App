e80f2cf3073c4ba69f1a78e79960999adc3e269e
Vang Vang <vang2code@gmail.com>
2023-06-23 17:20:51 -0500

    refactor LevelBadge to its own component, added jsdoc for what the function is

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,48 +1,52 @@
-import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector, useDispatch} from 'react-redux';
-import { useEffect } from 'react';
+import React from "react";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 // material UI grid components
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
 
-import './Dashboard.css'
+import "./Dashboard.css";
 
 // dashboard widget components
-import SkillTable from './DashboardWidgets/SkillTable';
-import Overall from './DashboardWidgets/Overall';
-import HeatmapChart from './DashboardWidgets/HeatmapChart/HeatmapChart';
-import ConsumedContent from './DashboardWidgets/ConsumedContent';
+import SkillTable from "./DashboardWidgets/SkillTable/SkillTable";
+import Overall from "./DashboardWidgets/Overall";
+import HeatmapChart from "./DashboardWidgets/HeatmapChart/HeatmapChart";
+import ConsumedContent from "./DashboardWidgets/ConsumedContent";
 
 function Dashboard() {
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
 
-    useEffect(() => {
-        dispatch({type: 'FETCH_USER_ACTIVITIES'})
-    }, [])
+  useEffect(() => {
+    dispatch({ type: "FETCH_USER_ACTIVITIES" });
+  }, []);
 
-    
   return (
-    
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2} direction="row" justifyContent='center' alignItems='left'>
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justifyContent="center"
+        alignItems="left"
+      >
         <Grid item xs={12} sm={12} md={10}>
-          <Paper sx={{mt:5}}>
+          <Paper sx={{ mt: 5 }}>
             <Overall />
           </Paper>
         </Grid>
         <Grid item xs={12} sm={12} md={7} mt={-2}>
-          <Paper  id="dboard-skill-table">
+          <Paper id="dboard-skill-table">
             <SkillTable />
           </Paper>
         </Grid>
-        <Grid item  xs={8} sm={8} md={4} mr={1} mt={-2}>
+        <Grid item xs={8} sm={8} md={4} mr={1} mt={-2}>
           <Paper>
-            <HeatmapChart/>
+            <HeatmapChart />
           </Paper>
           <Paper>
             <ConsumedContent />
--- src/components/Dashboard/DashboardWidgets/SkillTable/SkillTable.jsx
+++ src/components/Dashboard/DashboardWidgets/SkillTable/SkillTable.jsx
@@ -1,5 +1,4 @@
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useSelector} from "react-redux";
 
 // MUI Components
 import Table from "@mui/material/Table";
@@ -18,7 +17,10 @@ import { styled } from "@mui/material/styles";
 
 import "./SkillTable.css";
 // How do we calculate the level, current XP / XP to next level, etc.
-// How do we do badges?
+
+// Badge Component
+import LevelBadge from "./SkillTableComponents/LevelBadge";
+
 function SkillTable() {
   // things needed from the store to calculate the user's skill levels
   const skills = useSelector((store) => store.skills);
@@ -79,113 +81,6 @@ function SkillTable() {
     },
   }));
 
-  const levelBadge = (currentLevel) => {
-    switch (true) {
-      case currentLevel === 50:
-        return "҉ | 50 | ҉";
-      case currentLevel === 49:
-        return "««| 49 |»»";
-      case currentLevel === 48:
-        return "««| 48 |»»";
-      case currentLevel === 47:
-        return "««| 47 |»»";
-      case currentLevel === 46:
-        return "««| 46 |»»";
-      case currentLevel === 45:
-        return "««| 45 |»»";
-      case currentLevel === 44:
-        return "««| 44 |»»";
-      case currentLevel === 43:
-        return "««| 43 |»»";
-      case currentLevel === 42:
-        return "««| 42 |»»";
-      case currentLevel === 41:
-        return "««| 41 |»»";
-      case currentLevel === 40:
-        return "««| 40 |»»";
-      case currentLevel === 39:
-        return "‹«| 39 |»›";
-      case currentLevel === 38:
-        return "‹«| 38 |»›";
-      case currentLevel === 37:
-        return "‹«| 37 |»›";
-      case currentLevel === 36:
-        return "‹«| 36 |»›";
-      case currentLevel === 35:
-        return "‹«| 35 |»›";
-      case currentLevel === 34:
-        return "‹«| 34 |»›";
-      case currentLevel === 33:
-        return "‹«| 33 |»›";
-      case currentLevel === 32:
-        return "‹«| 32 |»›";
-      case currentLevel === 31:
-        return "‹«| 31 |»›";
-      case currentLevel === 30:
-        return "‹«| 30 |»›";
-      case currentLevel === 29:
-        return "‹«| 29 |»›";
-      case currentLevel === 28:
-        return "‹«| 28 |»›";
-      case currentLevel === 27:
-        return "‹«| 27 |»›";
-      case currentLevel === 26:
-        return "‹«| 26 |»›";
-      case currentLevel === 25:
-        return "‹«| 25 |»›";
-      case currentLevel === 24:
-        return "«| 24 |»";
-      case currentLevel === 23:
-        return "«| 23 |»";
-      case currentLevel === 22:
-        return "«| 22 |»";
-      case currentLevel === 21:
-        return "«| 21 |»";
-      case currentLevel === 20:
-        return "«| 20 |»";
-      case currentLevel === 19:
-        return "«| 19 |»";
-      case currentLevel === 18:
-        return "«| 18 |»";
-      case currentLevel === 17:
-        return "«| 17 |»";
-      case currentLevel === 16:
-        return "«| 16 |»";
-      case currentLevel === 15:
-        return "«| 15 |»";
-      case currentLevel === 14:
-        return "‹| 14 |›";
-      case currentLevel === 13:
-        return "‹| 13 |›";
-      case currentLevel === 12:
-        return "‹| 12 |›";
-      case currentLevel === 11:
-        return "‹| 11 |›";
-      case currentLevel === 10:
-        return "‹| 10 |›";
-      case currentLevel === 9:
-        return "‹| 9 |›";
-      case currentLevel === 8:
-        return "‹| 8 |›";
-      case currentLevel === 7:
-        return "‹| 7 |›";
-      case currentLevel === 6:
-        return "‹| 6 |›";
-      case currentLevel === 5:
-        return "‹| 5 |›";
-      case currentLevel === 4:
-        return "‹| 4 |›";
-      case currentLevel === 3:
-        return "‹| 3 |›";
-      case currentLevel === 2:
-        return "‹| 2 |›";
-      case currentLevel === 1:
-        return "‹| 1 |›";
-      case currentLevel === 0:
-        return "‹| 0 |›";
-    }
-  };
-
   return (
     <div>
       <h2>skills</h2>
@@ -234,7 +129,7 @@ function SkillTable() {
                     </Box>
                   </TableCell>
                   {/* Badge */}
-                  <TableCell align="center">{levelBadge(calculateLevel(skill))}</TableCell>
+                  <TableCell align="center">{LevelBadge(calculateLevel(skill))}</TableCell>
                 </TableRow>
               );
             })}
@@ -244,4 +139,5 @@ function SkillTable() {
     </div>
   );
 }
+
 export default SkillTable;
