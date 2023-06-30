90a3db2902e68d93c0edf2c2b6146644e118d958
Vang Vang <vang2code@gmail.com>
2023-06-30 11:47:19 -0500

    auto format barChart and Calendar

--- src/components/PageComponents/StatsPage/BarChartHorizontal.jsx
+++ src/components/PageComponents/StatsPage/BarChartHorizontal.jsx
@@ -1,69 +1,72 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 function BarChartHorizontal() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_USER_TOTAL_XP_POINTS' })
-    }, [])
+  useEffect(() => {
+    dispatch({ type: "FETCH_USER_TOTAL_XP_POINTS" });
+  }, []);
 
-    const userXpPoints = useSelector((store) => store.xpPoints)
+  const userXpPoints = useSelector((store) => store.xpPoints);
 
+  const options = {
+    indexAxis: "y",
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: true,
+        text: "Total XP Points",
+      },
+    },
+  };
 
-    const options = {
-        indexAxis: 'y',
-        elements: {
-            bar: {
-                borderWidth: 2,
-            },
-        },
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'bottom',
-            },
-            title: {
-                display: true,
-                text: 'Total XP Points',
-            },
-        },
-    };
+  const labels = userXpPoints.map((skill) => {
+    return skill.skill;
+  });
 
-    const labels = userXpPoints.map((skill) => { return (skill.skill) });
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "XP Skill Points",
+        data: userXpPoints.map((skill) => {
+          return skill.xp_points;
+        }),
+        borderColor: "rgb(100, 212, 253)",
+        backgroundColor: "rgba(100, 212, 253, 0.5)",
+      },
+    ],
+  };
 
-    const data = {
-        labels,
-        datasets: [
-            {
-                label: 'XP Skill Points',
-                data: userXpPoints.map((skill) => { return (skill.xp_points) }),
-                borderColor: 'rgb(100, 212, 253)',
-                backgroundColor: 'rgba(100, 212, 253, 0.5)',
-            },
-        ],
-    };
-
-    return (<Bar options={options} data={data} />);
+  return <Bar options={options} data={data} />;
 }
 export default BarChartHorizontal;
